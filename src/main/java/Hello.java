5650bb751617d51cc89613a9fb7bf7dd37472143
manshuk89 <manshuk.karasaeva@gmail.com>
2021-01-27 00:10:55 -0500

    movie1upd Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world,b21");
         System.out.println("B21 is awesome SDET");
     }
 }
